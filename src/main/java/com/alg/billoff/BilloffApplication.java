afdd5d077705d564206d2d5f902984e46fdbe895
Budes Alin <alin76alin@yahoo.com>
2023-04-02 18:02:06 +0300

    first commit

---
+++ src/main/java/com/alg/billoff/BilloffApplication.java
@@ -0,0 +1,13 @@
+package com.alg.billoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilloffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilloffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alg/billoff/BilloffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alg.billoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilloffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
